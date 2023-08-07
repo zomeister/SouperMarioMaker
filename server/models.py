e78949ea0b825a2024d0df309f5ec04eaa049c96
Zoe Guidry <oyezoey@gmail.com>
2023-08-07 12:04:10 -0500

    models and tables

--- server/models.py
+++ server/models.py
@@ -4,3 +4,40 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Soup(db.Model, SerializerMixin):
+    __tablename__ = 'soups'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    
+    soup_ingredients = db.relationship('SoupIngredient', back_populates='soup')
+    ingredients = association_proxy('soup_ingredients', 'ingredient')
+    
+    def __repr__(self):
+        return f"<id: {self.id}, {self.name}>"
+    
+
+class Ingredient(db.Model, SerializerMixin):
+    __tablename__ = 'ingredients'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    
+    soup_ingredients = db.relationship('SoupIngredient', back_populates='ingredient')
+    soups = association_proxy('soup_ingredients', 'soup')
+    
+    def __repr__(self):
+        return f"<id: {self.id}, {self.name}>"
+    
+class SoupIngredient(db.Model, SerializerMixin):
+    __tablename__ = 'soup_ingredients'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    soup_id = db.Column(db.Integer, db.ForeignKey('soups.id'))
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
+    
+    soup = db.relationship('Soup', back_populates='soup_ingredients')
+    ingredient = db.relationship('Ingredient', back_populates='soup_ingredients')
+    
+    def __repr__(self):
+        return f"<id: {self.id}, {self.name}>"
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,45 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Soup, Ingredient, SoupIngredient
+
+
+# def relate_records(soups, ingredients, soup_ingredients):
+#     for si  in soup_ingredients:
+#         si.soup = rc(soups)
+#         si.ingredient
+        
+fake = Faker()
+def delete_records():
+    with app.app_context():
+        Soup.query.delete()
+        Ingredient.query.delete()
+        SoupIngredient.query.delete()
+        db.session.commit()
+    
+def create_records():
+    with app.app_context():
+        
+        soups = []
+        for i in range(10):
+            soup = Soup(name=fake.first_name())
+            soups.append(soup)
+        db.session.add_all(soups)
+        db.session.commit()
+        
+        ingredients = []
+        # for i in range(10):
+        #     ingredient = Ingredient(fake.first_name())
+        #     ingredients.append(ingredient)
+        # db.session.add_all(ingredients)
+        
+        
+        return soups # + ingredients
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
         print("Starting seed...")
+        # delete_records()
+        soups = create_records()
+        
         # Seed code goes here!
